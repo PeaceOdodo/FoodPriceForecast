4805f16823e47876b25b18ad9bb95cc241ff5abd
Onoyiza <ododopeace@gmail.com>
2024-11-13 01:41:59 +0100

    Update design

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ if st.button("Predict"):
     
     if prediction is not None:
         with st.container():
-            st.markdown(f"<div class='prediction-text'>Prediction for {food_item} in {state} on {prediction_date}</div>", unsafe_allow_html=True)
+            st.markdown(f"<div class='prediction-text'>Forecast for {food_item} in {state} by {prediction_date}</div>", unsafe_allow_html=True)
             st.write(f"**Predicted Price**: ₦{round(prediction['yhat']/10) * 10}")
             st.write(f"**Minimum Price Range**: ₦{round(prediction['yhat_lower']/10) * 10}")
             st.write(f"**Maximum Price Range**: ₦{round(prediction['yhat_upper']/10) * 10}")
